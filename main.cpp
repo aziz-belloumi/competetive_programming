3642066485b498432681d87f9074999148ecf0c5
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-04 04:39:48 +0100

    magical sticks

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <set>
 int main() {
     int t;
     std::cin >> t;
     while (t--) {
         int n;
         std::cin >> n;
-        std::set<long> container ;
-        for(int i = 0 ; i < n ; i++) {
-            long a ;
-            std::cin >> a ;
-            container.insert(a);
-        }
-        container.size() == n ? std::cout << "YES"<< std::endl : std::cout << "NO"<< std::endl ;
+        std::cout << (n + 1) / 2 << std::endl;
     }
     return 0;
 }
