41cd9655e74c5cef2fcec939444bc5446b0755ef
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-04 05:43:38 +0100

    blank space

--- main.cpp
+++ main.cpp
@@ -5,7 +5,24 @@ int main() {
     while (t--) {
         int n;
         std::cin >> n;
-        std::cout << (n + 1) / 2 << std::endl;
+        int max = 0;
+        int currentLength = 0;
+        for (int i = 0; i < n; i++) {
+            int a;
+            std::cin >> a;
+            if (a == 0) {
+                currentLength++;
+            } else {
+                if (max < currentLength) {
+                    max = currentLength;
+                }
+                currentLength = 0;
+            }
+        }
+        if (max < currentLength) {
+            max = currentLength;
+        }
+        std::cout << max << std::endl;
     }
     return 0;
 }
