349cac18a46dd1b12ce7a87d6745ce3226bf7a1e
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-14 21:22:43 +0100

    integer game

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <cmath>
+using namespace std;
 int main() {
-    std::string name;
-    std::cin >> name;
-    int total_rotations = 0;
-    char current_position = 'a';
-    for (char target : name) {
-        int clockwise_distance = abs(target - current_position);
-        int counterclockwise_distance = 26 - clockwise_distance;
-        total_rotations += std::min(clockwise_distance, counterclockwise_distance);
-        current_position = target;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n % 3 == 1 || n % 3 == 2) {
+            cout << "First" << endl;
+        }
+        else {
+            cout << "Second" << endl;
+        }
     }
-    std::cout << total_rotations << std::endl;
     return 0;
 }
