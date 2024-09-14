d23030cac6a702fa6fc8f1929b94bbfede392c13
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-14 21:58:49 +0100

    maximum increasing

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        if (n % 3 == 1 || n % 3 == 2) {
-            cout << "First" << endl;
+    int n;
+    std::cin >> n ;
+    std::vector<long> arr(n);
+    for(int i = 0 ; i < n ; i++) {
+        std::cin >> arr[i];
+    }
+    int k = 1 ;
+    int maximum_len = 0 ;
+    for(int i = 0 ; i < n - 1 ; i++) {
+        if(arr[i] < arr[i+1]) {
+            k++ ;
         }
         else {
-            cout << "Second" << endl;
+            if(maximum_len < k ) {
+                maximum_len = k ;
+            }
+            k = 1 ;
         }
     }
+    if(maximum_len < k) {
+        maximum_len = k;
+    }
+    std::cout << maximum_len << std::endl ;
     return 0;
 }
