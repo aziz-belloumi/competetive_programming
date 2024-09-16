16c0da9b9a466a9253bafa922290a172204d55f0
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-16 11:55:45 +0100

    ordinary numbers

--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
-#include <vector>
+int countOrdinaryNumbers(int n) {
+    int count = 0;
+    for (int digit = 1; digit <= 9; ++digit) {
+        int ordinaryNumber = digit;
+        while (ordinaryNumber <= n) {
+            count++;
+            ordinaryNumber = ordinaryNumber * 10 + digit;
+        }
+    }
+    return count;
+}
 int main() {
-    int n;
-    std::cin >> n ;
-    std::vector<long> arr(n);
-    for(int i = 0 ; i < n ; i++) {
-        std::cin >> arr[i];
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int n;
+        std::cin >> n;
+        std::cout << countOrdinaryNumbers(n) << std::endl;
     }
-    int k = 1 ;
-    int maximum_len = 0 ;
-    for(int i = 0 ; i < n - 1 ; i++) {
-        if(arr[i] < arr[i+1]) {
-            k++ ;
-        }
-        else {
-            if(maximum_len < k ) {
-                maximum_len = k ;
-            }
-            k = 1 ;
-        }
-    }
-    if(maximum_len < k) {
-        maximum_len = k;
-    }
-    std::cout << maximum_len << std::endl ;
     return 0;
 }
