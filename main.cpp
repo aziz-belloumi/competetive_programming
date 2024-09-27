96ea31f3ac588d4ce3bea4bca612a0590a079e5c
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-27 12:15:27 +0100

    multiple sum

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-long long countOrdinaryNumbers(long long n) {
-    long long count = 0;
-    for (int digit = 1; digit <= 9; ++digit) {
-        long long ordinaryNumber = digit;
-        while (ordinaryNumber <= n) {
-            count++;
-            ordinaryNumber = ordinaryNumber * 10 + digit;
+int find_optimal_x(int n) {
+    int optimal_x = 2 ;
+    int max_sum = 0 ;
+    for(int x = 2 ; x <= n ; ++x) {
+        int k = n / x ;
+        int total_sum = x * k * (k+1) / 2 ;
+        if(total_sum > max_sum){
+            max_sum = total_sum ;
+            optimal_x = x ;
         }
     }
-    return count;
+    return optimal_x ;
 }
 int main() {
-    int t;
-    std::cin >> t;
-    while (t--) {
-        long long n;
+    int t ;
+    std::cin >> t ;
+    while(t--) {
+        int n;
         std::cin >> n;
-        std::cout << countOrdinaryNumbers(n) << std::endl;
+        std::cout << find_optimal_x(n) << std::endl;
     }
     return 0;
 }
